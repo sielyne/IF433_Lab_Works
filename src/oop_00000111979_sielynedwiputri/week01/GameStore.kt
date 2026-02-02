0af5db891336cad9c6626757479abafce581b48f
sielyne <sielyne.dwi@student.umn.ac.id>
2026-02-02 19:42:47 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000111979_sielynedwiputri/week01/GameStore.kt
+++ src/oop_00000111979_sielynedwiputri/week01/GameStore.kt
@@ -3,4 +3,7 @@ package oop_00000111979_sielynedwiputri.week01
 fun main() {
     val gameTitle: String = "Grounded"
     val price: Int = 550000
+    val totalPrice: Int = calculateDiscount(price)
 }
+
+fun calculateDiscount(price: Int): Int = if (price < 500000) (price*90)/100 else (price*80)/100

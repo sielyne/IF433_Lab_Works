9752e6e156d85dacddd8353a89035dac9ae53286
sielyne <sielyne.dwi@student.umn.ac.id>
2026-02-02 16:06:56 +0700

    week01: add calculateStatus function

--- src/oop_00000111979_sielynedwiputri/week01/GradingSystem.kt
+++ src/oop_00000111979_sielynedwiputri/week01/GradingSystem.kt
@@ -13,5 +13,8 @@ fun main(){
         else -> "D"
     }
     println("Grade kamu: $grade")
+    println("Status: ${calculateStatus(score)}")
 }
+
+fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
 

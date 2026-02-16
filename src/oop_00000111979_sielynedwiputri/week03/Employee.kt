07c2a5f69170e44aab392535d80a0d60b9be4451
sielyne <sielyne.dwi@student.umn.ac.id>
2026-02-16 15:30:53 +0700

    week03: create basic Employee class

---
+++ src/oop_00000111979_sielynedwiputri/week03/Employee.kt
@@ -0,0 +1,9 @@
+package oop_00000111979_sielynedwiputri.week03
+
+class Employee(val name: String) {
+    var salary: Int = 0
+        set(value) {
+            println("Mencoba set gaji ke: $value")
+            this.salary = value
+        }
+}

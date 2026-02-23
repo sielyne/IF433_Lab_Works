ceac662e5d93706f061c1791e7daf47f822a181e
sielyne <sielyne.dwi@student.umn.ac.id>
2026-02-23 14:52:09 +0700

    week04: test Vehicle and Car in main

---
+++ src/oop_00000111979_sielynedwiputri/week04/Main.kt
@@ -0,0 +1,14 @@
+package oop_00000111979_sielynedwiputri.week04
+
+fun main() {
+    println("--- Testing Vehicle ---")
+    val generalVehicle = Vehicle("Sepeda Onthel")
+    generalVehicle.honk()
+    generalVehicle.accelerate()
+
+    println("--- Testing Car ---")
+    val myCar = Car("Toyota", 4)
+    myCar.openTrunk()
+    myCar.honk()
+    myCar.accelerate()
+}

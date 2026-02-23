e821682b7f4ef1608841ecbed2ffe1cfdd25a97d
sielyne <sielyne.dwi@student.umn.ac.id>
2026-02-23 15:13:32 +0700

    week04: (task 1) test ElectricCar in main

--- src/oop_00000111979_sielynedwiputri/week04/ElectricCar.kt
+++ src/oop_00000111979_sielynedwiputri/week04/ElectricCar.kt
@@ -1,4 +1,7 @@
 package oop_00000111979_sielynedwiputri.week04
 
-class ElectricCar {
+class ElectricCar (brand: String, numberOfDoors: Int, val batteryCapacity: Int) : Car(brand, numberOfDoors) {
+    final override fun accelerate() {
+        println("brand berakselerasi dalam sunyi. Kapasitas baterai: $batteryCapacity%.")
+    }
 }
--- src/oop_00000111979_sielynedwiputri/week04/Main.kt
+++ src/oop_00000111979_sielynedwiputri/week04/Main.kt
@@ -11,4 +11,10 @@ fun main() {
     myCar.openTrunk()
     myCar.honk()
     myCar.accelerate()
+
+    println("--- Testing Electric Car ---")
+    val electricCar = ElectricCar("Mio", 2, 55)
+    electricCar.accelerate()
+    electricCar.honk()
+    electricCar.openTrunk()
 }

77114a3e32199407c7ae14461be25570f9598029
sielyne <sielyne.dwi@student.umn.ac.id>
2026-03-02 17:19:24 +0700

    week05: (task 1) test MathHelper overloading in main

--- src/oop_00000111979_sielynedwiputri/week05/MathHelper.kt
+++ src/oop_00000111979_sielynedwiputri/week05/MathHelper.kt
@@ -1,2 +1,15 @@
 package oop_00000111979_sielynedwiputri.week05
 
+class MathHelper {
+    fun hitungLuas(sisi: Int): Int {
+        return sisi * sisi
+    }
+
+    fun hitungLuas(panjang: Int, lebar: Int): Int {
+        return panjang * lebar
+    }
+
+    fun hitungLuas(jariJari: Double): Double {
+        return 3.14 * jariJari * jariJari
+    }
+}
---
+++ src/oop_00000111979_sielynedwiputri/week05/MathMain.kt
@@ -0,0 +1,11 @@
+package oop_00000111979_sielynedwiputri.week05
+
+fun main() {
+    fun main() {
+        val coba1 = MathHelper()
+
+        coba1.hitungLuas(6)
+        coba1.hitungLuas(7.1)
+        coba1.hitungLuas(3, 2)
+    }
+}

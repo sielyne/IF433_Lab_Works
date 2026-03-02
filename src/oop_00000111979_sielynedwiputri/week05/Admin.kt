bcfd0a6aca0d4a837b1225b0269fbe49b4894c9e
sielyne <sielyne.dwi@student.umn.ac.id>
2026-03-02 14:49:48 +0700

    week05: (task 1) create MathHelper class with overloading

--- src/oop_00000111979_sielynedwiputri/week05/Admin.kt
+++ src/oop_00000111979_sielynedwiputri/week05/Admin.kt
@@ -1,2 +1,11 @@
 package oop_00000111979_sielynedwiputri.week05
 
+class Admin(nama: String) : Pegawai(nama) {
+    override fun bekerja() {
+        println("[$nama] sedang duduk di depan komputer melayani administrasi.")
+    }
+
+    fun doAdminWork() {
+        println("[$nama] sedang merekap data absensi mahasiswa.")
+    }
+}
--- src/oop_00000111979_sielynedwiputri/week05/Dosen.kt
+++ src/oop_00000111979_sielynedwiputri/week05/Dosen.kt
@@ -1,2 +1,11 @@
 package oop_00000111979_sielynedwiputri.week05
 
+class Dosen(nama: String, val nidn: String): Pegawai(nama) {
+    override fun bekerja() {
+        println("[$nama] sedang menyiapkan materi perkuliahan dan merevisi RPKPS.")
+    }
+
+    fun mengajar() {
+        println("[$nama] sedang mengajar mahasiswa di kelas.")
+    }
+}

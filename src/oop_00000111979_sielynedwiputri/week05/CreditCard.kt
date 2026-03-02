8302f2c707b89f18c2d6c68c12e2f2c83a354fa6
sielyne <sielyne.dwi@student.umn.ac.id>
2026-03-02 17:28:57 +0700

    week05: (task 2) create CreditCard class

---
+++ src/oop_00000111979_sielynedwiputri/week05/CreditCard.kt
@@ -0,0 +1,10 @@
+package oop_00000111979_sielynedwiputri.week05
+
+class CreditCard(accountName: String, val limit: Double, var usedAmount: Double = 0.0) : PaymentMethod(accountName) {
+    override fun processPayment(amount: Double) {
+        if(amount + usedAmount <= limit) {
+            usedAmount += amount
+            println("Pemakaian Credit Card berhasil! akumulasi saldo terpakai: Rp. $usedAmount ")
+        } else println("Transaksi melebihi limit, proses ditolak.")
+    }
+}

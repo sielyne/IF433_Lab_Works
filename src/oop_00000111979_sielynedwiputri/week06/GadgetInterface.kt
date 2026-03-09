cf52a9065dc00066b8d479e7849d2f77b7c1e035
sielyne <sielyne.dwi@student.umn.ac.id>
2026-03-09 13:33:27 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000111979_sielynedwiputri/week06/GadgetInterface.kt
@@ -0,0 +1,9 @@
+package oop_00000111979_sielynedwiputri.week06
+
+interface Camera {
+    fun turnOn() { println("Lensa kamera terbuka dan sensor aktif.") }
+}
+
+interface Phone {
+    fun turnOn() { println("Sinyal seluler mencari jaringan.") }
+}

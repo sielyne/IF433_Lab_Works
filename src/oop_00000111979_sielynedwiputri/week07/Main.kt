069dfcf0c031ca055a2d64475169765df5f650a3
sielyne <sielyne.dwi@student.umn.ac.id>
2026-03-16 15:45:14 +0700

    week07: test Singleton and Companion Object in main

--- src/oop_00000111979_sielynedwiputri/week07/Main.kt
+++ src/oop_00000111979_sielynedwiputri/week07/Main.kt
@@ -1,3 +1,11 @@
 package oop_00000111979_sielynedwiputri.week07
 
-val client = NetworkClient("https://api.umn.ac.id")
+fun main() {
+    println("=== TEST SINGLETON ===")
+    println("Status: ${DatabaseManager.connectionStatus}")
+    DatabaseManager.connect()
+
+    println("\n=== TEST COMPANION OBJECT ===")
+    val client = NetworkClient.createClient()
+    client.connect()
+}

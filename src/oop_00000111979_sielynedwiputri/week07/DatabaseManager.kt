4b6353d54eca2026d285e1487c82c39f32c4af4e
sielyne <sielyne.dwi@student.umn.ac.id>
2026-03-16 14:18:39 +0700

    week07: create singleton DatabaseManager

---
+++ src/oop_00000111979_sielynedwiputri/week07/DatabaseManager.kt
@@ -0,0 +1,10 @@
+package oop_00000111979_sielynedwiputri.week07
+
+object DatabaseManager {
+    var connectionStatus: String = "Disconnected"
+
+    fun connect() {
+        connectionStatus = "Connected to Server"
+        println("Database is ready.")
+    }
+}

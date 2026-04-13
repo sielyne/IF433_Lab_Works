ad7a4121b0d987ae8641a72bf5095351b06484ca
sielyne <sielyne.dwi@student.umn.ac.id>
2026-04-13 17:06:47 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000111979_sielynedwiputri/Week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000111979_sielynedwiputri.Week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}

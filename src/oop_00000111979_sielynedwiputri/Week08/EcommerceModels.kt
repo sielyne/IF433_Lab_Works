aaf11ef5fd4776cf3960f17458e0136f0ee57a7c
sielyne <sielyne.dwi@student.umn.ac.id>
2026-04-13 16:58:05 +0700

    week08: (task) create sealed class Product and data classes

---
+++ src/oop_00000111979_sielynedwiputri/Week08/EcommerceModels.kt
@@ -0,0 +1,8 @@
+package oop_00000111979_sielynedwiputri.Week08
+
+sealed class Product
+
+data class Electronic(val id: String, val name: String, val warrantyMonths: Int) : Product()
+
+data class Clothing(val id: String, val name: String, val size: String) : Product()
+

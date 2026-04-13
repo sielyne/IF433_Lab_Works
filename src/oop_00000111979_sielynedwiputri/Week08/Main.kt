bac987000a00e12196fd0f9bd50f27d9ca8ab853
sielyne <sielyne.dwi@student.umn.ac.id>
2026-04-13 14:08:36 +0700

    week08: create mixed data collection

--- src/oop_00000111979_sielynedwiputri/Week08/Main.kt
+++ src/oop_00000111979_sielynedwiputri/Week08/Main.kt
@@ -16,4 +16,13 @@ fun main() {
     } ?: "Transaksi invalid: Harga belum di-set!"
 
     println(receipt)
+
+    println("\n=== TEST SAFE CASTING ===")
+    val mixedData: List<Any> = listOf(
+        "Smartphone",
+        1500000,
+        UserProfile("Andi", null),
+        "Laptop",
+        4500000.0
+    )
 }

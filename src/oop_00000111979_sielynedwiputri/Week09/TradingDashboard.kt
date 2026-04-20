8e05cf7767d941ec064b0e874f63364f70f16197
sielyne <sielyne.dwi@student.umn.ac.id>
2026-04-20 15:17:24 +0700

    week09: (task) chain sortedByDescending and map for top performers

--- src/oop_00000111979_sielynedwiputri/Week09/TradingDashboard.kt
+++ src/oop_00000111979_sielynedwiputri/Week09/TradingDashboard.kt
@@ -3,15 +3,21 @@ package oop_00000111979_sielynedwiputri.Week09
 fun main() {
     val tradeHistory = listOf(
         TradeLog("BTCUSDT", "LONG", 20, 1.2, "CLOSED"),
-        TradeLog("BTCUSDT", "SHORT", 15, -3.7, "OPEN"),
-        TradeLog("ETHUSDT", "LONG", 13, 4.7, "OPEN"),
-        TradeLog("ETHUSDT", "SHORT", 17, -5.1, "OPEN"),
-        TradeLog("BTCUSDT", "SHORT", 25, -6.7, "CLOSED"),
-        TradeLog("ETHUSDT", "LONG", 11, 8.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 15, -3.7, "OPEN"),
+        TradeLog("BTCUSDT", "LONG", 13, 4.7, "CLOSED"),
+        TradeLog("SOLUSDT", "SHORT", 17, -5.1, "OPEN"),
+        TradeLog("ETHUSDT", "SHORT", 25, -6.7, "CLOSED"),
+        TradeLog("BTCUSDT", "LONG", 11, 8.5, "CLOSED"),
 
     )
 
     val closedTrades = tradeHistory.filter { it.status == "CLOSED" }
     val winningTrades = closedTrades.filter { it.roe > 0 }
     val losingTrades = closedTrades.filter { it.roe <= 0 }
+
+    val topPerformersString = winningTrades
+        .sortedByDescending { it.roe }
+        .map { "WIN [${it.pair} - ${it.position}]: +${it.roe}% ROE (Lev: ${it.leverage}x)"
+        }
+
 }

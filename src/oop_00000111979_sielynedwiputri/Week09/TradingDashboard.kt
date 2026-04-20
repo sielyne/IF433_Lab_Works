0502b4c59d3eb3e218b188866033511080ccbba3
sielyne <sielyne.dwi@student.umn.ac.id>
2026-04-20 15:24:29 +0700

    week09: (task) print top performers using forEach

--- src/oop_00000111979_sielynedwiputri/Week09/TradingDashboard.kt
+++ src/oop_00000111979_sielynedwiputri/Week09/TradingDashboard.kt
@@ -26,4 +26,9 @@ fun main() {
         }
 
     val uniquePairs = tradeHistory.map {it.pair}.toSet()
+
+    println("=== CRYPTO TRADING DASHBOARD ===")
+    topPerformersString.forEach {
+        println(it)
+    }
 }

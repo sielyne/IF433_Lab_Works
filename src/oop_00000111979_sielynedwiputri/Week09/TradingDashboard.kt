a056ab5cfe0cb81c81d922a1b69eae0030208382
sielyne <sielyne.dwi@student.umn.ac.id>
2026-04-20 15:22:34 +0700

    week09: (task) extract unique trading pairs using map and toSet

--- src/oop_00000111979_sielynedwiputri/Week09/TradingDashboard.kt
+++ src/oop_00000111979_sielynedwiputri/Week09/TradingDashboard.kt
@@ -24,4 +24,6 @@ fun main() {
         .sortedBy { it.roe }
         .map { "LOSS [${it.pair} - ${it.position}]: +${it.roe}% ROE (Lev: ${it.leverage}x)"
         }
+
+    val uniquePairs = tradeHistory.map {it.pair}.toSet()
 }

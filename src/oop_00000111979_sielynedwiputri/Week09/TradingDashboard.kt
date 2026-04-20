6aab5fa47e0fa4f2cec2a3b0d831d53ca66f8604
sielyne <sielyne.dwi@student.umn.ac.id>
2026-04-20 15:03:00 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_00000111979_sielynedwiputri/Week09/TradingDashboard.kt
@@ -0,0 +1,13 @@
+package oop_00000111979_sielynedwiputri.Week09
+
+fun main() {
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 1.2, "CLOSED"),
+        TradeLog("BTCUSDT", "SHORT", 15, -3.7, "OPEN"),
+        TradeLog("ETHUSDT", "LONG", 13, 4.7, "OPEN"),
+        TradeLog("ETHUSDT", "SHORT", 17, -5.1, "OPEN"),
+        TradeLog("BTCUSDT", "SHORT", 25, -6.7, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 11, 8.5, "CLOSED"),
+
+    )
+}

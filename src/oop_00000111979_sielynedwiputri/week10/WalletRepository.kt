52ad80b1345a18bb3c6c658e9b47542ddbb99a02
sielyne <sielyne.dwi@student.umn.ac.id>
2026-04-27 15:19:46 +0700

    week10: (task) implement generic WalletRepository

---
+++ src/oop_00000111979_sielynedwiputri/week10/WalletRepository.kt
@@ -0,0 +1,11 @@
+package oop_00000111979_sielynedwiputri.week10
+
+class WalletRepository<T> {
+    private val items = mutableListOf<T>()
+    fun add(item: T) {
+        items.add(item)
+    }
+    fun getAll() : List<T> {
+        return items
+    }
+}

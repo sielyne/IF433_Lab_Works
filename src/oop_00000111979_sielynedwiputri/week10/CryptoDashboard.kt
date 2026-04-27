6272302a79f59472f6e5af5edf07b3b7964003a8
sielyne <sielyne.dwi@student.umn.ac.id>
2026-04-27 15:29:07 +0700

    week10: (task) populate coin repository

--- src/oop_00000111979_sielynedwiputri/week10/CryptoDashboard.kt
+++ src/oop_00000111979_sielynedwiputri/week10/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_00000111979_sielynedwiputri.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 12.5))
+    coinRepo.add(Coin("ETH", 50.2))
+    coinRepo.add(Coin("USDT", 85.7))
 }

adcb5ebca213d351e7c6d629cd89e573be047029
sielyne <sielyne.dwi@student.umn.ac.id>
2026-05-04 15:22:39 +0700

    week11: combine apply and also in object setup

--- src/oop_00000111979_sielynedwiputri/week11/ScopeConfiguration.kt
+++ src/oop_00000111979_sielynedwiputri/week11/ScopeConfiguration.kt
@@ -3,22 +3,17 @@ package oop_00000111979_sielynedwiputri.week11
 data class User(var name: String = "", var age: Int = 0)
 
 fun main() {
-    println("=== TEST APPLY ===")
-    val user = User().apply {
-        name = "Alex"
-        age = 25
-    }
+    val user = User(name = "Alex", age = 25)
     println(user)
-
-    println("\n=== TEST ALSO ===")
-    val numbers = mutableListOf(1, 2, 3)
-    numbers.also {
-        println("Log Sebelum ditambah: $it")
-    }.add(4)
-    println("Setelah ditambah: $numbers")
 
     println("\n=== TEST WITH ===")
     with(user) {
         println("User Detail -> Nama: $name, Umur: $age")
+    }
+
+    val newUser = User("Budi", 20).apply {
+        age = 21
+    }.also {
+        println("User baru berhasil dibuat: $it")
     }
 }

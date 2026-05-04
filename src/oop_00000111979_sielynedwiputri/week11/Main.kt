fbdfbff13eb61fe91042675205998373ad543347
sielyne <sielyne.dwi@student.umn.ac.id>
2026-05-04 14:41:21 +0700

    week11: test extension functions in main

---
+++ src/oop_00000111979_sielynedwiputri/week11/Main.kt
@@ -0,0 +1,10 @@
+package oop_00000111979_sielynedwiputri.week11
+
+fun main() {
+    println("=== TEST EXTENSION FUNCTIONS ===")
+    println("Alex".addGreeting())
+    println("Hi".repeatTimes(3))
+
+    val text: String? = null
+    println("Apakah null/empty? ${text.isNullOrEmptyCustom()}")
+}

04ccce6a8ecd795c0d56073e8aba7f8e24e4c4c3
sielyne <sielyne.dwi@student.umn.ac.id>
2026-05-04 15:35:26 +0700

    week11: (task) configure Ezviz camera with also

--- src/oop_00000111979_sielynedwiputri/week11/SmartDashboard.kt
+++ src/oop_00000111979_sielynedwiputri/week11/SmartDashboard.kt
@@ -6,4 +6,13 @@ fun main() {
     SmartDevice("Philips WiZ Living Room", "Lighting", true, 12)
         .apply{}
         .also{homeDevices.add(it)}
+
+    SmartDevice("Ezviz Outdoor", "Camera")
+        .apply {
+            isOnline = true
+            powerLoad = 5}
+        .also {
+            println("(LOG) Kamera terhubung")
+            homeDevices.add(it)}
 }
+

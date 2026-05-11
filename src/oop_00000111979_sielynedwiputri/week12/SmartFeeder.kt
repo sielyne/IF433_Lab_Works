2e7e6d40a0ef3403f411a856d120454e2e057d43
sielyne <sielyne.dwi@student.umn.ac.id>
2026-05-11 13:57:03 +0700

    week12: (task) implement hardware validation with throw

--- src/oop_00000111979_sielynedwiputri/week12/SmartFeeder.kt
+++ src/oop_00000111979_sielynedwiputri/week12/SmartFeeder.kt
@@ -4,5 +4,10 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
     require(requestedGram > 0) {
         "Porsi kibble harus lebih dari 0 gr"
     }
+
+    if (isJammed) {
+        throw DispenserJamException()
+    }
+
     return availableGram - requestedGram
 }

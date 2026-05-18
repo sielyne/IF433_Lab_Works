ce9e660baaa6e87b5853921784d8b8752b202fff
sielyne <sielyne.dwi@student.umn.ac.id>
2026-05-18 13:35:39 +0700

    week13: implement basic writeText function

---
+++ src/oop_00000111979_sielynedwiputri/week13/BasicFile10.kt
@@ -0,0 +1,9 @@
+package oop_00000111979_sielynedwiputri.week13
+import java.io.File
+
+fun main() {
+    println("=== TEST WRITE TEXT ===")
+    val file  = File("notes.txt")
+    file.writeText("Line 1: Inisialisasi sistem.\n")
+    println("Fle berhasil dibuat dan ditulis.")
+}

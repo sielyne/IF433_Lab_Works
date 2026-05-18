7e5aaaaa1de12a8ad1d422452ad24ca082846ba4
sielyne <sielyne.dwi@student.umn.ac.id>
2026-05-18 15:27:49 +0700

    week13: (task) implement toCsv for TradeRecord

--- src/oop_00000111979_sielynedwiputri/week13/CryptoTradeLogger.kt
+++ src/oop_00000111979_sielynedwiputri/week13/CryptoTradeLogger.kt
@@ -1,3 +1,7 @@
 package oop_00000111979_sielynedwiputri.week13
 
 data class TradeRecord(val id: Int, val symbol: String, val type: String, val margin: Double, val pnl: Double)
+
+fun TradeRecord.toCsv(): String {
+    return "$id,$symbol,$type,$margin,$pnl"
+}

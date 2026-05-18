60af69635e53ae422825b33d97f8b477b8ff3643
sielyne <sielyne.dwi@student.umn.ac.id>
2026-05-18 15:19:38 +0700

    week13: implement appendText operation

--- src/oop_00000111979_sielynedwiputri/week13/BasicFile10.kt
+++ src/oop_00000111979_sielynedwiputri/week13/BasicFile10.kt
@@ -6,4 +6,7 @@ fun main() {
     val file  = File("notes.txt")
     file.writeText("Line 1: Inisialisasi sistem.\n")
     println("Fle berhasil dibuat dan ditulis.")
+
+    file.appendText("Line 2: Menambahkan konfigurasi baru.")
+    println("Teks berhasil di-append.")
 }

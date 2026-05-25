9c06e7ceee64effda309c1f18605a33161df63fe
sielyne <sielyne.dwi@student.umn.ac.id>
2026-05-25 15:08:53 +0700

    week14: add SeniorDiscount without modifying calculator

--- src/oop_00000111979_sielynedwiputri/week14/OcpRefactored.kt
+++ src/oop_00000111979_sielynedwiputri/week14/OcpRefactored.kt
@@ -5,13 +5,17 @@ interface DiscountStrategy {
 }
 
 class StudentDiscount : DiscountStrategy {
-    override fun apply(price: Double): Double = price*0.80
+    override fun apply(price: Double) = price*0.80
 }
 
 class MemberDiscount : DiscountStrategy {
-    override fun apply(price: Double): Double = price*0.85
+    override fun apply(price: Double) = price*0.85
 }
 
 class SafeDiscountCalculator(private val strategy: DiscountStrategy) {
     fun calculate(price: Double) = strategy.apply(price)
+}
+
+class SeniorDiscount : DiscountStrategy {
+    override fun apply(price: Double) = price * 0.75
 }
